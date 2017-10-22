707c99933aaec1b38ba4093ef1cbf2d848d42915
Brian Santoso <btsantoso@gmail.com>
2017-10-21 19:54:46 -0700

    updated

--- nn2.py
+++ nn2.py
@@ -4,8 +4,13 @@
 #
 
 import numpy as np
+import copy
 
-np.random.seed(5465456)
+# Neural Net does not work with this seed?
+# np.random.seed(5465456)
+
+# Neural Net works fine with this seed
+# np.random.seed(2342342)
 
 def sigmoid(x):
 	return 1.0 / (1.0 + np.exp(-x))
@@ -48,13 +53,16 @@ class NeuralNet:
 
 	def back_propagation(self, outputs, target, rate):
 
+		# output is the last vector in the outputs array
 		output = outputs[self.num_of_layers - 1]
+		# initialize deltas array with delta_k at the end
 		deltas = [output * (1 - output) * (output - target)]
 
+		# start at second to last layer and stop at the 2nd layer
 		for l in range(self.num_of_layers - 2, 0, -1):
 			# delta_j = output_j * (1 - output_j) * sum(delta_k * weight_jk)
-			o_j = outputs[l]
-			delta = o_j * (1 - o_j) * np.dot(self.weights[l].transpose(), deltas[0])
+			o_j = outputs[l] #outputs of corresponding layer
+			delta = o_j * (1 - o_j) * np.dot(self.weights[l].T, deltas[0])
 			deltas.insert(0, delta)
 
 		partial_derivatives = []
@@ -62,12 +70,12 @@ class NeuralNet:
 			partial_derivatives.append(deltas[l - 1][:,None] * outputs[l - 1])
 
 		for l in range(1, self.num_of_layers):
-			self.weights[l - 1] = self.weights[l - 1] - rate * partial_derivatives[l - 1]
-			self.biases[l - 1] = self.biases[l - 1] - rate * deltas[l - 1]
+			self.weights[l - 1] -= rate * partial_derivatives[l - 1]
+			self.biases[l - 1] -= rate * deltas[l - 1]
 
-		return
+		return partial_derivatives
 
-	def train(self, training_data, rate, epoch, min_error = 1e-6, printinfo = False):
+	def train(self, training_data, rate, epoch, min_error = 1e-4, printinfo = 1e99):
 
 		d = 0
 		error = 1e99
@@ -78,14 +86,13 @@ class NeuralNet:
 			inp = training_data[i][0]
 			target = training_data[i][1]
 			outputs = self.feed_forward(inp)
-
-			error = mean_squared_error(outputs[-1:], target)
+			error = mean_squared_error(outputs[self.num_of_layers - 1], target)
 
 			self.back_propagation(outputs, target, rate)
 
 			d += 1
 		
-			if printinfo and d % 10 == 0:
+			if d % printinfo == 0:
 				print('epoch: ', d, ' error: ', error)
 
 		return
@@ -94,26 +101,104 @@ class NeuralNet:
 		print('sizes: {0} \nnum_of_layers: {1} \nweights: {2} \nbiases: {3} \n'.format(self.sizes, self.num_of_layers, self.weights, self.biases))
 
 
-training_data = [ 	[np.array([0, 0]), np.array([0])],
-					[np.array([1, 0]), np.array([1])],
-					[np.array([0, 1]), np.array([1])],
-					[np.array([1, 1]), np.array([0])]	]
+xor_training_data = [ 	[np.array([0, 0]), np.array([0])],
+						[np.array([1, 0]), np.array([1])],
+						[np.array([0, 1]), np.array([1])],
+						[np.array([1, 1]), np.array([0])]	]
 
-nn = NeuralNet((2, 2, 1))
-nn.train(training_data, 10, 2000, 1e-4)
+xor = NeuralNet((2, 2, 1))
+xor.train(xor_training_data, 5, 5000, 1e-4, printinfo=1000)
+
+# Testing the net
 
 print('tests:')
-o = nn.feed_forward(np.array([0, 0]))
-print('output: ', o[-1:])
-o = nn.feed_forward(np.array([1, 0]))
-print('output: ', o[-1:])
-o = nn.feed_forward(np.array([0, 1]))
-print('output: ', o[-1:])
-o = nn.feed_forward(np.array([1, 1]))
-print('output: ', o[-1:])
+o = xor.feed_forward(np.array([0, 0]))
+print('input: (0, 0) 	output: ', o[-1:][0])
+o = xor.feed_forward(np.array([1, 0]))
+print('input: (1, 0) 	output: ', o[-1:][0])
+o = xor.feed_forward(np.array([0, 1]))
+print('input: (0, 1) 	output: ', o[-1:][0])
+o = xor.feed_forward(np.array([1, 1]))
+print('input: (1, 1) 	output: ', o[-1:][0])
 
-nn.info()
 
-# a possible XOR Solution
-# nn.weights = [np.array([[20, 20], [-20, -20]]), np.array([[20, 20]])]
-# nn.biases = [np.array([-10, 30]), np.array([-30])]
+# Debugging - Numerical Approximation of Gradient Descent
+
+# EPSILON = 1e-4
+
+# inp = xor_training_data[0][0]
+# target = xor_training_data[0][1]
+# outputs = xor.feed_forward(inp)
+# gradient = xor.back_propagation(outputs, target, 0)	# partial derivatives of each weight
+# print(gradient)
+
+
+# print('weights ', xor.weights)
+
+# t1 = copy.deepcopy(xor.weights)
+# t2 = copy.deepcopy(xor.weights)
+# t1[1][0][0] += EPSILON
+# print('t1 ', t1)
+
+
+# t2[1][0][0] -= EPSILON
+# print('t2 ', t2)
+
+# xor.weights = t1
+# J1 = xor.feed_forward(xor_training_data[0][0])[-1:][0]
+# xor.weights = t2
+# J2 = xor.feed_forward(xor_training_data[0][0])[-1:][0]
+
+# partial_derivative_approximation = (mean_squared_error(J1, target) - mean_squared_error(J2, target)) / (2 * EPSILON)
+# print(partial_derivative_approximation)
+
+
+
+# Handwritten Digit Recognition
+# from mnist import MNIST
+# mndata = MNIST('samples')
+# images, labels = mndata.load_training()
+# #images, labels = mndata.load_testing()
+
+# hdr_training_data = []
+
+# for index in range(0, len(labels)):
+
+# 	input = np.array(images[index]) / 255
+# 	# input = np.array(images[index])
+# 	output = np.zeros(10)
+# 	output[labels[index]] = 1.0
+
+# 	hdr_training_data.append([input, output])
+
+
+# hdr = NeuralNet((784, 50, 10))
+
+# hdr.train(hdr_training_data, 15, 5000, printinfo = 100)
+
+# print('expected: ', hdr_training_data[1][1])
+# o = hdr.feed_forward(hdr_training_data[1][0])
+# print('output: ', o[-1:][0])
+
+
+# print(mndata.display(images[25]))
+# print(labels[25])
+
+
+# reverse_training_data = [ 	[np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0]), np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])],
+# 							[np.array([0, 1, 0, 0, 0, 0, 0, 0, 0, 0]), np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 0])],
+# 							[np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0]), np.array([0, 0, 0, 0, 0, 0, 0, 1, 0, 0])],
+# 							[np.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0]), np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0])],
+# 							[np.array([0, 0, 0, 0, 1, 0, 0, 0, 0, 0]), np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0])],	
+# 							[np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0]), np.array([0, 0, 0, 0, 1, 0, 0, 0, 0, 0])],
+# 							[np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0]), np.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0])],
+# 							[np.array([0, 0, 0, 0, 0, 0, 0, 1, 0, 0]), np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])],
+# 							[np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]), np.array([0, 1, 0, 0, 0, 0, 0, 0, 0, 0])],
+# 							[np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1]), np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])]	]
+
+# reverse = NeuralNet((10, 10))
+# reverse.train(reverse_training_data, 10, 100, 1e-4, True)
+
+# print('tests:')
+# o = reverse.feed_forward(np.array([0, 1, 0, 1, 0, 1, 0, 0, 0, 1]))
+# print('output: ', o[-1:][0])
